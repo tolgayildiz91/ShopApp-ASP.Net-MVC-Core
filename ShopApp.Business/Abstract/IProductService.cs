2dc50dfde7255f4efa32737c752adcd3c8efee7a
Tolga Yıldız <tolgayildiz91@hotmail.com.tr>
2023-03-02 02:55:05 +0300

    validation işlemleri yapıldı.

--- ShopApp.Business/Abstract/IProductService.cs
+++ ShopApp.Business/Abstract/IProductService.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace ShopApp.Business.Abstract
 {
-    public interface IProductService
+    public interface IProductService:IValidator<Product>
     {
         List<Product> GetAll();
         List<Product> GetProductsByCategory(string category, int page,int pageSize);
         Product GetById(int id);
         Product GetProductDetails(int id);
-        void Create(Product entity);
+        bool Create(Product entity);
         void Delete(Product entity);
         void Update(Product entity);
         int GetCountByCategory(string category);
